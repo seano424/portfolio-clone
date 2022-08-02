3decfbc8c9262a8a95b3a6c1d2b5fa51f4eda48b
Sean OReilly <soreilly424@gmail.com>
2022-08-02 16:12:26 -0500

    Footer And Icons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import SocialLinks from './SocialLinks'
+import GitHub from '@/icons/GitHub'
+import LinkedIn from '@/icons/LinkedIn'
+import Twitter from '@/icons/Twitter'
+import { socialLinks } from 'utils/constants'
 
 export default function AboutMe() {
   return (
@@ -22,7 +25,22 @@ export default function AboutMe() {
         Hi! 👋 My name is Sean. I build websites with modern tools and
         technologies!
       </h1>
-      <SocialLinks />
+      <div className="flex space-x-8 justify-center">
+        {socialLinks.map((link) => (
+          <Link href={link.href} key={link.name}>
+            <a
+              className={link.classes}
+              aria-label={link.name}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.icon === 'GitHub' && <GitHub className="w-8 h-8" />}
+              {link.icon === 'Twitter' && <Twitter className="w-8 h-8" />}
+              {link.icon === 'LinkedIn' && <LinkedIn className="w-8 h-8" />}
+            </a>
+          </Link>
+        ))}
+      </div>
       <div className="flex gap-10 justify-center">
         <Link href="/">
           <a className="button">See my work</a>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import { projects } from 'utils/projects'
 import Image from 'next/image'
 import Link from 'next/link'
+import GitHub from '../public/icons/GitHub'
 interface Props {
   heading?: string
 }
@@ -36,20 +37,7 @@ export default function Projects(props: Props) {
               </Link>
               <Link href={projects[0].githubUrl}>
                 <a className="border-4 rounded-full p-3 flex justify-center items-center">
-                  <svg
-                    className="hover:text-primary hover:scale-105 transition-all duration-300 ease-linear text-emerald-500 dark:text-green-200 dark:hover:text-primary font-black h-8 w-8"
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                  </svg>
+                  <GitHub className="hover:text-primary hover:scale-105 transition-all duration-300 ease-linear text-emerald-500 dark:text-green-200 dark:hover:text-primary font-black h-8 w-8" />
                 </a>
               </Link>
             </div>
