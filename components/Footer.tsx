b8429f0a70f0244fb2050d9204e781ca241eb0a8
Sean OReilly <soreilly424@gmail.com>
2022-08-08 11:55:55 -0500

    Footer Text and Links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,17 +3,20 @@ import { socialLinks, navLinks } from 'utils/constants'
 import GitHub from '@/icons/GitHub'
 import Twitter from '@/icons/Twitter'
 import LinkedIn from '@/icons/LinkedIn'
+import { scrollTo } from 'utils/functions'
 
 export default function Footer() {
   return (
     <footer className="px-base py-base bg-light dark:bg-dark flex flex-col gap-8 items-center justify-center">
-      <div className="flex gap-10">
+      <div className="flex gap-5 justify-center lg:gap-10">
         {navLinks.map((link) => (
-          <Link key={link.title} href={link.url}>
-            <a className="text-xl text-dark dark:text-light hover:text-gray-600">
-              {link.title}
-            </a>
-          </Link>
+          <button
+            key={link.title}
+            className="text-dark dark:text-light hover:text-gray-600 text-lg lg:text-xl"
+            onClick={() => scrollTo(link.url)}
+          >
+            {link.title}
+          </button>
         ))}
       </div>
       <div className="flex gap-8 justify-center">
@@ -25,9 +28,9 @@ export default function Footer() {
               target="_blank"
               rel="noreferrer"
             >
-              {link.icon === 'GitHub' && <GitHub className="w-8 h-8" />}
-              {link.icon === 'Twitter' && <Twitter className="w-8 h-8" />}
-              {link.icon === 'LinkedIn' && <LinkedIn className="w-8 h-8" />}
+              {link.icon === 'GitHub' && <GitHub className="w-6 h-6 lg:w-8 lg:h-8" />}
+              {link.icon === 'Twitter' && <Twitter className="w-6 h-6 lg:w-8 lg:h-8" />}
+              {link.icon === 'LinkedIn' && <LinkedIn className="w-6 h-6 lg:w-8 lg:h-8" />}
             </a>
           </Link>
         ))}
