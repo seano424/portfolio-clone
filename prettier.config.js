aa7c83ecb3b50ba7047dccfdf027ade966658307
Sean OReilly <soreilly424@gmail.com>
2022-08-17 09:21:40 -0500

    Updating Prettier Config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  trailingComma: "es5",
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
-};
+}
