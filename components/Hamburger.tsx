7c732628fab44c02d04c2be8bbd4fe4abf96680a
Sean OReilly <soreilly424@gmail.com>
2022-08-23 13:38:38 -0500

    Updating Hamburger Color State

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -1,18 +1,23 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { MobileContext } from 'contexts/MobileContext'
 import { useTheme } from 'next-themes'
 import { Squash as Burger } from 'hamburger-react'
 
 const Hamburger = () => {
   const { theme } = useTheme()
+  const [color, setColor] = useState('white')
   const { open, toggle } = useContext(MobileContext)
+
+  useEffect(() => {
+    setColor(theme === 'dark' ? 'white' : 'black')
+  }, [theme])
 
   return (
     <button aria-label="Toggle Mobile Menu" className="lg:hidden">
       <Burger
         label="Show Menu"
         rounded
-        color={theme === 'light' ? 'black' : 'white'}
+        color={color}
         size={24}
         toggled={open}
         toggle={toggle}
