91505c5c65eb198107253966c7918543c8586bdc
Sean OReilly <soreilly424@gmail.com>
2022-08-24 18:43:03 -0500

    Updated Font On Mobile

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -29,32 +29,32 @@ export default function Menu() {
           }}
           className="fixed top-20 z-20 flex min-h-screen w-10/12 items-start justify-center bg-light/90 py-4 shadow-2xl filter backdrop-blur-sm dark:bg-dark/90 md:w-2/3 lg:hidden"
         >
-          <ul className="px-base mt-10 grid gap-8 sm:gap-16">
+          <div className="px-base mt-10 grid gap-8 sm:gap-16">
             {mobileNavLinks.map((link, i) => (
-              <li key={i}>
-                <button
-                  className={`text-4xl font-black uppercase tracking-tighter text-fuchsia-400 drop-shadow-2xl transition-all ease-linear hover:text-dark dark:hover:text-light sm:text-6xl
-                    ${i === 0 && 'duration-200'}
-                    ${i === 1 && 'duration-300'}
-                    ${i === 2 && 'duration-500'}
-                    ${i === 3 && 'duration-700'}
-                  `}
-                  onClick={() => handleNavigation(link.url)}
-                >
-                  {link.title}
-                </button>
-              </li>
+              <button
+                key={link.title}
+                aria-label={`Scroll Button To Section ${link.title}`}
+                className={`text-left text-4xl font-black uppercase tracking-tighter text-fuchsia-400 drop-shadow-2xl transition-all ease-linear hover:text-dark dark:hover:text-light sm:text-6xl md:text-7xl
+                ${i === 0 && 'duration-200'}
+                ${i === 1 && 'duration-300'}
+                ${i === 2 && 'duration-500'}
+                ${i === 3 && 'duration-700'}
+              `}
+                onClick={() => handleNavigation(link.url)}
+              >
+                {link.title}
+              </button>
             ))}
             <Link
               href="mailto:soreilly424@gmail.com"
               target="_blank"
               rel="noreferrer noopener"
             >
-              <a className="text-4xl font-black uppercase tracking-tighter text-fuchsia-400 drop-shadow-2xl transition-all ease-linear hover:text-dark dark:hover:text-light sm:text-6xl">
+              <a className="text-4xl font-black uppercase tracking-tighter text-fuchsia-400 drop-shadow-2xl transition-all ease-linear hover:text-dark dark:hover:text-light sm:text-6xl md:text-7xl">
                 Contact
               </a>
             </Link>
-          </ul>
+          </div>
         </m.div>
       )}
     </AnimatePresence>
