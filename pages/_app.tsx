93b89bd266647cbc427a068dc52659ca710ff173
Sean OReilly <soreilly424@gmail.com>
2022-09-05 16:15:08 -0500

    Moving Lots of Providers to Index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
-import { LazyMotion, domAnimation } from 'framer-motion'
 import '../styles/globals.css'
-import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <LazyMotion features={domAnimation}>
-      <RecoilRoot>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </RecoilRoot>
-    </LazyMotion>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,23 @@ import Hero from '@/components/Hero'
 import AboutMe from '@/components/AboutMe'
 import Projects from '@/components/Projects'
 import Layout from '@/components/Layout'
+import { LazyMotion, domAnimation } from 'framer-motion'
+import { RecoilRoot } from 'recoil'
+import { ThemeProvider } from 'next-themes'
 
 const Home = () => {
   return (
-    <Layout>
-      <Hero />
-      <Projects />
-      <AboutMe />
-    </Layout>
+    <LazyMotion features={domAnimation}>
+      <ThemeProvider attribute="class">
+        <RecoilRoot>
+          <Layout>
+            <Hero />
+            <Projects />
+            <AboutMe />
+          </Layout>
+        </RecoilRoot>
+      </ThemeProvider>
+    </LazyMotion>
   )
 }
 
