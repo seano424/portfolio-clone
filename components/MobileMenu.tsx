713d29faa5ea8aefac6e325b2a3e022ad4063664
Sean OReilly <soreilly424@gmail.com>
2022-09-06 13:45:39 -0500

    Updating Mobile Menu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link'
 import { AnimatePresence, m } from 'framer-motion'
-import { mobileNavLinks } from 'lib/links'
 import useShowMobileNav from 'hooks/useShowMobileNav'
+import { mobileNavLinks } from 'lib/links'
 import scrollTo from 'lib/scrollTo'
 
 export default function Menu() {
   const [showMobileNav, setShowMobileNav] = useShowMobileNav()
-
   const handleNavigation = async (url) => {
     scrollTo(url, 900, -80)
     setShowMobileNav((prev) => !prev)
+  }
+  const mobileMenu = {
+    hidden: { opacity: 0, x: -200 },
+    show: { opacity: 1, x: 0 },
+    exit: {
+      opacity: 0,
+      x: -600,
+      transition: {
+        duration: 0.6,
+        ease: [0.83, 0, 0.17, 1],
+      },
+    },
   }
 
   return (
@@ -17,43 +28,33 @@ export default function Menu() {
       {showMobileNav && (
         <m.div
           key="mobileMenu"
-          initial={{ opacity: 0, x: -200 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{
-            opacity: 0,
-            x: -600,
-            transition: {
-              duration: 0.6,
-              ease: [0.83, 0, 0.17, 1],
-            },
-          }}
-          className="fixed top-20 z-20 flex min-h-screen w-10/12 items-start justify-center bg-light/90 py-4 shadow-2xl filter backdrop-blur-sm dark:bg-dark/90 md:w-2/3 lg:hidden"
+          variants={mobileMenu}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+          className="px-base py-base fixed top-20 z-20  flex min-h-screen w-10/12 flex-col gap-8 bg-light/90 shadow-2xl filter backdrop-blur-sm dark:bg-dark/90 md:w-2/3 lg:hidden"
         >
-          <div className="px-base grid gap-8 sm:gap-16">
-            {mobileNavLinks.map((link, i) => (
-              <button
-                key={link.title}
-                aria-label={`Scroll Button To Section ${link.title}`}
-                className={`p-1 text-left text-5xl font-black uppercase tracking-tighter text-fuchsia-400 drop-shadow-2xl transition-all ease-linear hover:text-secondary dark:text-white dark:hover:text-light sm:text-6xl md:text-7xl
+          {mobileNavLinks.map((link, i) => (
+            <button
+              key={link.title}
+              aria-label={`Scroll Button To Section ${link.title}`}
+              className={`mobile-link
                 ${i === 0 && 'duration-200'}
                 ${i === 1 && 'duration-300'}
                 ${i === 2 && 'duration-500'}
               `}
-                onClick={() => handleNavigation(link.url)}
-              >
-                {link.title}
-              </button>
-            ))}
-            <Link
-              href="mailto:soreilly424@gmail.com"
-              target="_blank"
-              rel="noreferrer noopener"
+              onClick={() => handleNavigation(link.url)}
             >
-              <a className="text-5xl font-black uppercase tracking-tighter text-fuchsia-400 drop-shadow-2xl transition-all duration-700 ease-linear hover:text-secondary dark:text-white dark:hover:text-light sm:text-6xl md:text-7xl">
-                Contact
-              </a>
-            </Link>
-          </div>
+              {link.title}
+            </button>
+          ))}
+          <Link
+            href="mailto:soreilly424@gmail.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <a className="mobile-link duration-700">Contact</a>
+          </Link>
         </m.div>
       )}
     </AnimatePresence>
