de7c2fa6a49b8bfb57e4d713f0e539f5a47909ee
Sean OReilly <soreilly424@gmail.com>
2022-10-11 14:28:59 -0600

    moving containers to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from 'next/app'
+import { LazyMotion, domAnimation } from 'framer-motion'
+import { RecoilRoot } from 'recoil'
+import { ThemeProvider } from 'next-themes'
+import Layout from '@/components/Layout'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LazyMotion features={domAnimation}>
+      <ThemeProvider attribute="class">
+        <RecoilRoot>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </ThemeProvider>
+    </LazyMotion>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,14 @@
 import Hero from '@/components/Hero'
 import AboutMe from '@/components/AboutMe'
 import Projects from '@/components/Projects'
-import Layout from '@/components/Layout'
-import { LazyMotion, domAnimation } from 'framer-motion'
-import { RecoilRoot } from 'recoil'
-import { ThemeProvider } from 'next-themes'
 
 const Home = () => {
   return (
-    <LazyMotion features={domAnimation}>
-      <ThemeProvider attribute="class">
-        <RecoilRoot>
-          <Layout>
-            <Hero />
-            <Projects />
-            <AboutMe />
-          </Layout>
-        </RecoilRoot>
-      </ThemeProvider>
-    </LazyMotion>
+    <>
+      <Hero />
+      <Projects />
+      <AboutMe />
+    </>
   )
 }
 
